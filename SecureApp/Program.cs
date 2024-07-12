21f526913a6e68a7ff0e0230e6bf0426d90ddad4
MASON_DANA <gaming04.life@gmail.com>
2024-07-12 10:33:37 -0600

    Recent Test on loca machine.

--- SecureApp/Program.cs
+++ SecureApp/Program.cs
@@ -1,2 +1,25 @@
-﻿
-Console.WriteLine("Hellooooooo World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinancialAid
+{
+    internal static class Program
+    {
+        
+        // Main function, simply declares a new User superclass and creates an instance of the User Form.
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            User user = new User();
+
+            Application.Run(new UserForm(user));
+        }
+    }
+}
